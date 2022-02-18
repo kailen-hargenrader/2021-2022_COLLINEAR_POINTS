16cdbbc89cdce5e14a35c077273bdf80561992a1
kailen-hargenrader <khargenrader@maret.org>
2022-02-18 13:56:46 -0500

    feat : Implement BruteColPoints W/o Reduction

--- src/main/java/BruteCollinearPoints.java
+++ src/main/java/BruteCollinearPoints.java
@@ -11,19 +11,45 @@
  *   between p and r, and between p and s are all equal.
  *
  *************************************************************************/
+import java.util.Arrays;
+import java.util.ArrayList;
 public class BruteCollinearPoints {
-   
+  private ArrayList<LineSegment> lines; // Container for line segments before conversion to array
+  
   public BruteCollinearPoints(Point[] points) {
      // finds all line segments containing 4 points
+	 if(points == null)throw new IllegalArgumentException("Points cannot be null.");
+	 for(int i = 0; i<points.length; i++) {
+		 if(points[i] == null) throw new IllegalArgumentException("Points cannot be null.");
+		 for(int j = points.length-1; j>i; j--) {
+			 if(points[i] == points[j]) throw new IllegalArgumentException("Points must be Different.");
+		 }
+	 }
+	 lines = new ArrayList<LineSegment>();
+	 Arrays.sort(points);
+	 double slope;
+	 for(int i = 0; i<points.length-3; i++) {					//This is really gross
+		 for(int j = i+1; j<points.length-2; j++) {
+			 slope = points[i].slopeTo(points[j]);
+			 for(int k = j+1; k<points.length-1; k++) {
+				 if(slope == points[j].slopeTo(points[k])) {
+					 for(int m = k+1; m<points.length; m++) {
+						 if(slope == points[k].slopeTo(points[m])) lines.add(new LineSegment(points[i],points[m]));
+					 } 
+				 }
+				 
+			 }
+		 }
+	 }
    }
    
    public int numberOfSegments() {
      // the number of line segments
-     return 0;
+     return lines.size();
    }
    
    public LineSegment[] segments() {
      // the line segments
-     return null;
+     return lines.toArray(new LineSegment[lines.size()]);
    }
 }
--- src/test/java/BruteCollinearPointsTest.java
+++ src/test/java/BruteCollinearPointsTest.java
@@ -9,33 +9,42 @@ import org.junit.Test;
 import edu.princeton.cs.algs4.In;
 
 public class BruteCollinearPointsTest {
-  
-  BruteCollinearPoints bcp;
-  @Before
-  public void setUp() throws Exception {
-    bcp = generateBCP("input10.txt");
-  }
-  
-  private BruteCollinearPoints generateBCP(String filename) {
-    In in = new In("collinear-test-files/" + filename);
-    int n = in.readInt();
-    Point[] points = new Point[n];
-    for (int i = 0; i < n; i++) {
-      int x = in.readInt();
-      int y = in.readInt();
-      points[i] = new Point(x, y);
-    } 
-    return new BruteCollinearPoints(points);
-  }
 
-  @Test
-  public void testNumberOfSegments() {
-    fail("Not yet implemented");
-  }
+	BruteCollinearPoints bcp10_2;
+	BruteCollinearPoints bcp20_5;
+	BruteCollinearPoints bcp56_20;
+	@Before
+	public void setUp() throws Exception {
+		bcp10_2 = generateBCP("input10.txt");
+		bcp20_5 = generateBCP("input20.txt");
+		bcp56_20 = generateBCP("input56.txt");
+	}
 
-  @Test
-  public void testSegments() {
-    fail("Not yet implemented");
-  }
+	private BruteCollinearPoints generateBCP(String filename) {
+		In in = new In("collinear-test-files/" + filename);
+		int n = in.readInt();
+		Point[] points = new Point[n];
+		for (int i = 0; i < n; i++) {
+			int x = in.readInt();
+			int y = in.readInt();
+			points[i] = new Point(x, y);
+		} 
+		return new BruteCollinearPoints(points);
+	}
+
+	@Test
+	public void testNumberOfSegments() {
+		for(LineSegment line: bcp10_2.segments()) {
+			System.out.println(line);
+		}
+		assertTrue("number of segments should be 2, was " + bcp10_2.numberOfSegments(), bcp10_2.numberOfSegments() == 2);
+		assertTrue("number of segments should be 5, was " + bcp20_5.numberOfSegments(), bcp20_5.numberOfSegments() == 2);
+		assertTrue("number of segments should be 20, was " + bcp56_20.numberOfSegments(), bcp56_20.numberOfSegments() == 2);
+	}
+
+	@Test
+	public void testSegments() {
+		fail("Not yet implemented");
+	}
 
 }
